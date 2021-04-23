86b00c60f4dc7868886d88dec021cf861ce901c1
Abisai Rodriguez <abisai.rodriguez13@gmail.com>
2021-04-23 16:20:11 -0700

    first commit

---
+++ pages/about.vue
@@ -0,0 +1,6 @@
+<template>
+  <div>
+    <h1>About Page</h1>
+    <NuxtLink to="/">Home page</NuxtLink>
+  </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <h1 class="web-header">Digital Reflection</h1>
+    <p class="routes">Hello There</p>
+    <NuxtLink to="/about" class="text-blue-500">About page</NuxtLink>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+
